4c5868609695858ac7c96a8f85ed33a338e1c4b0
Maitham-Ali <20186039@stu.uob.edu.bh>
2024-12-03 17:05:01 +0300

    profile

--- Final code/profile.php
+++ Final code/profile.php
@@ -23,7 +23,7 @@ $passwordRegex = "/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9_#@%*\\-]{8,24}$/
 <body>
     <div class="nav">
         <div class="logo">
-            <p><a href="homepage.php"> Logo</a></p>
+            <p><a href="homepage.php"> UOB</a></p>
         </div>
         <div class="right-links">
             <a href="profile.php">Change Profile</a>
@@ -41,27 +41,66 @@ $passwordRegex = "/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9_#@%*\\-]{8,24}$/
                 $password = $_POST['password'];
                 $confirm_password = $_POST['confirm_password'];
                 $profile_pic = $_FILES['profile_pic'];
-                
                 $id = $_SESSION['userid'];
+                $check = $db->prepare("SELECT * FROM user WHERE id=?");
+                $check->bindParam(1,$id);
+                $check->execute();
+                while($r= $check->fetch()){
+                    if(empty($email)){
+                        $email = $r['Email'];
+                    }
+                    $currentEmail = $r['Email'];
+                    
+                    if(empty($password))
+                    {
+                        $password= $r["Password"];
+                        $confirm_password = $password;
 
+                    }
+
+                    
+                    if ($_FILES["profile_pic"]["error"] === UPLOAD_ERR_NO_FILE) {
+                        $profile_pic_folder = $r['pfp']; }
+                    
+                    
+                }  
+                
+                
                 if (!preg_match($emailRegex, $email)) {
                     echo "<div class='message'>
-                        <p>Please enter a valid email address.</p>
-                    </div> <br>";
+                        <p>Please enter a valid email address .</p>
+                    </div> <br>";echo "<a href='profile.php'><button class='btn'>Go Back</button>";
                 } elseif (!preg_match($passwordRegex, $password)) {
                     echo "<div class='message'>
                         <p>Please enter a valid password (8-25 characters, at least one uppercase letter, one lowercase letter, and one special character).</p>
-                    </div> <br>";
+                    </div> <br>";echo "<a href='profile.php'><button class='btn'>Go Back</button>";
                 } elseif ($password !== $confirm_password) {
                     echo "<div class='message'>
                         <p>Passwords do not match.</p>
-                    </div> <br>";
+                    </div> <br>";echo "<a href='profile.php'><button class='btn'>Go Back</button>";
+                    
                 } else {
+                    if(!empty($_POST['email'])){
+                    $check = $db->prepare("SELECT * FROM user where Email =?");
+                    $check->bindParam(1,$email);
+                    $check->execute();
+                    if($check->rowCount() > 0){
+                        echo "<div class='message'>
+                        <p>The Email: $email is already used so it won't be updated other than the email.</p>
+                    </div> <br>";
+                    $email = $currentEmail;
+                    }
+                    
+                }
+
+                if(!$_FILES["profile_pic"]["error"] > 0){
                     $profile_pic_name = $profile_pic['name'];
                     $profile_pic_tmp_name = $profile_pic['tmp_name'];
                     $profile_pic_folder = 'images/' . $profile_pic_name;
                     
                     move_uploaded_file($profile_pic_tmp_name, $profile_pic_folder);
+                }
+
 
                     $edit_query = $db->prepare("UPDATE user SET Email=?, Password=?, pfp=? WHERE id=?");
                     $edit_query->bindParam(1, $email);
@@ -72,13 +111,15 @@ $passwordRegex = "/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9_#@%*\\-]{8,24}$/
 
                     if ($edit_query) {
                         echo "<div class='message'>
-                        <p>Profile Updated!</p>
+                        <p>Profile Updated! </p>
                     </div> <br>";
                         echo "<a href='profile.php'><button class='btn'>Go Home</button>";
                     }
                 }
-            } else {
-                $id = $_SESSION['id'];
+            }
+        
+            else {
+                $id = $_SESSION['userid'];
                 $query = $db->prepare("SELECT Email, pfp FROM user WHERE id=?");
                 $query->bindParam(1, $id);
                 $query->execute();
@@ -88,16 +129,22 @@ $passwordRegex = "/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9_#@%*\\-]{8,24}$/
                     $res_Profile_Pic = $result['pfp'];
                 }
             ?>
-            <header>Change Profile</header>
+            <header class="page-title">Change Profile</header>
+            <div class="profile-picture"> <img src="<?php echo $res_Profile_Pic; ?>" alt="Profile Picture"> </div>
+            
             <form action="" method="post" enctype="multipart/form-data">
-                <div class="field input">
-                    <label for="email">Email</label>
-                    <input type="text" name="email" id="email" value="<?php echo $res_Email; ?>" autocomplete="off" >
-                </div>
+                
+            <div class="field input profile-picture-update"> 
+
+                <label for="profile_pic">Update Profile Picture</label> 
+
+            <input type="file" name="profile_pic" id="profile_pic"> 
+        </div>
+
 
                 <div class="field input">
-                    <label for="profile_pic">Profile Picture</label>
-                    <input type="file" name="profile_pic" id="profile_pic" >
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" placeholder="your current email: <?php echo $res_Email; ?>" autocomplete="off" >
                 </div>
 
                 <div class="field input">
